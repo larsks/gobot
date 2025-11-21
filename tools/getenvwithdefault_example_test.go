2e373d6d1c5305a4deea8c07142fcbbbb4f98ee8
Lars Kellogg-Stedman <lars@oddbit.com>
2025-11-21 10:53:23 -0500

    doc: Update comments for GetenvWithDefault example

--- tools/getenvwithdefault_example_test.go
+++ tools/getenvwithdefault_example_test.go
@@ -17,6 +17,13 @@ func ExampleGetenvWithDefault() {
 	// because it supports more flag types than the stock flag module.
 	flag.IntVar(&intOption, "intoption", GetenvWithDefault("MY_INT_OPTION", 10), "an example int option")
 	flag.BoolVar(&boolOption, "booloption", GetenvWithDefault("MY_BOOL_OPTION", true), "an example bool option")
-	flag.TimeVar(&timeOption, "timeoption", GetenvWithDefault("MY_TIME_OPTION", defaultTime, "2006-01-02"), []string{"2006-01-02"}, "an example time option")
+	flag.TimeVar(&timeOption, "timeoption",
+		// We need to provide a time format for parsing the environment variable
+		// as a third parameter to ExampleGetenvWithDefault.
+		GetenvWithDefault("MY_TIME_OPTION", defaultTime, "2006-01-02"),
+
+		// pflag also needs a list of time formats for parsing command line options
+		[]string{"2006-01-02"},
+		"an example time option")
 	flag.Parse()
 }
