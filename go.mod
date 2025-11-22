aa03216580b6a8100e7f6f0c78396cdfd39df870
Lars Kellogg-Stedman <lars@oddbit.com>
2025-11-22 08:57:30 -0500

    doc: Rewrite example to remove dependencies

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/larsks/gobot
 
 go 1.24.5
-
-require github.com/spf13/pflag v1.0.10
--- tools/getenvwithdefault_example_test.go
+++ tools/getenvwithdefault_example_test.go
@@ -1,9 +1,8 @@
 package tools
 
 import (
+	"fmt"
 	"time"
-
-	flag "github.com/spf13/pflag"
 )
 
 func ExampleGetenvWithDefault() {
@@ -11,19 +10,11 @@ func ExampleGetenvWithDefault() {
 	var intOption int
 	var timeOption time.Time
 
-	defaultTime, _ := time.Parse("2006-01-02", "1900-01-01")
+	defaultTime, _ := time.Parse("2006-01-01", "2000-10-10")
 
-	// Note that we are using pflag (https://pkg.go.dev/github.com/spf13/pflag) in this example
-	// because it supports more flag types than the stock flag module.
-	flag.IntVar(&intOption, "intoption", GetenvWithDefault("MY_INT_OPTION", 10), "an example int option")
-	flag.BoolVar(&boolOption, "booloption", GetenvWithDefault("MY_BOOL_OPTION", true), "an example bool option")
-	flag.TimeVar(&timeOption, "timeoption",
-		// We need to provide a time format for parsing the environment variable
-		// as a third parameter to ExampleGetenvWithDefault.
-		GetenvWithDefault("MY_TIME_OPTION", defaultTime, "2006-01-02"),
+	intOption = GetenvWithDefault("MY_INT_OPTION", 10)
+	boolOption = GetenvWithDefault("MY_BOOL_OPTION", true)
+	timeOption = GetenvWithDefault("MY_TIME_OPTION", defaultTime, "2006-01-02")
 
-		// pflag also needs a list of time formats for parsing command line options
-		[]string{"2006-01-02"},
-		"an example time option")
-	flag.Parse()
+	fmt.Printf("int var is %d, bool var is %t, time var is %s", intOption, boolOption, timeOption)
 }
